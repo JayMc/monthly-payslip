facb0fad30b4b73f7d7a6ebee5f20d5008ad53f4
Jason McIver <mciver.jason@gmail.com>
2015-03-21 14:14:18 +1100

    fixed calc test

--- tests/test-calculations.js
+++ tests/test-calculations.js
@@ -19,9 +19,20 @@ describe('calculations', function(){
 		});
 	})
 
-	it('it should ', function(done){
+	it('it should fail on illegal negative super rate', function(done){
 
 		taxCal.calcAmount(82000, -3, function(err, taxInfo){
+
+			(err !== null).should.be.true;
+			(taxInfo === null).should.be.true;
+
+			done();
+		});
+	})
+
+	it('it should fail on illegal string super rate', function(done){
+
+		taxCal.calcAmount(82000, '8', function(err, taxInfo){
 
 			(err !== null).should.be.true;
 			(taxInfo === null).should.be.true;
